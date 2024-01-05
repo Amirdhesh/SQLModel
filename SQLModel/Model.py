4d9fc60b1f43af2fa4d9f9052d09fe7ee0b5a36d
Amirdhesh <Amirdhesh@users,noreply.github.com>
2024-01-05 22:38:22 +0530

    intial

--- SQLModel/Model.py
+++ SQLModel/Model.py
@@ -1,8 +1,8 @@
 from typing import Optional
-from sqlmodel import Field ,  SQLModel
+from sqlmodel import Field, SQLModel
 
 
-class students(SQLModel , table = True):
-    id : Optional[int] = Field(default=None , primary_key=True)
-    name : str
-    mail : str = Field(unique=True)
+class students(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    mail: str = Field(unique=True)
